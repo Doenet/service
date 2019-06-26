543450e557e3f027282f03ad5215211318e8f2c8
Jim Fowler <fowler@math.osu.edu>
2019-06-25 20:36:43 -0400

    testing multiple users

--- test/learner.spec.js
+++ test/learner.spec.js
@@ -16,11 +16,18 @@ before(helper.before);
 after(helper.after);
 
 describe("PUT /learners/:learner/progress", function() {
+  var user;
+  
   before(() => {
-    let user = new userModel({ name: 'Learner', email:'learner@name.com', password: 'abcde' });
+    user = new userModel({ name: 'Learner', email:'learner@name.com', password: 'abcde' });
     return user.save();
   });
 
+  before(() => {
+    let user = new userModel({ name: 'Another Learner', email:'learner2@name.com', password: 'abcde' });
+    return user.save();
+  });  
+  
   let agent = chai.request.agent(app);
   
   before(() => {
@@ -29,6 +36,14 @@ describe("PUT /learners/:learner/progress", function() {
       .auth('learner@name.com', 'abcde');
   });
 
+  let anotherAgent = chai.request.agent(app);
+  
+  before(() => {
+    return anotherAgent
+      .get("/users/learner2@name.com/token")
+      .auth('learner2@name.com', 'abcde');
+  });
+  
   it("saves progress", function() {
     return agent
       .put("/learners/me/progress")
@@ -46,9 +61,17 @@ describe("PUT /learners/:learner/progress", function() {
       });
   });
 
+  it("does not let other people view progress", function() {
+    return anotherAgent
+      .get("/learners/" + user._id + "/progress")
+      .then(function (res) {
+        expect(res).to.have.status(403);        
+      });
+  });
+  
   it("saves progress", function() {
     return agent
-      .put("/learners/me/progress")
+      .put("/learners/" + user._id + "/progress")
       .send({score:0.57})
       .then(function (res) {
         expect(res.body.score).to.eql(0.57);
@@ -57,7 +80,7 @@ describe("PUT /learners/:learner/progress", function() {
 
   it("still saves progress", function() {
     return agent
-      .get("/learners/me/progress")
+      .get("/learners/" + user._id + "/progress")    
       .then(function (res) {
         expect(res.body.score).to.eql(0.57);
       });
@@ -66,5 +89,9 @@ describe("PUT /learners/:learner/progress", function() {
   after( () => {
     return agent.close();
   });
+
+  after( () => {
+    return anotherAgent.close();
+  });  
 
 });
