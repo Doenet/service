617565d472f90d694e555f8463685066999507ae
Jim Fowler <fowler@math.osu.edu>
2019-12-09 09:56:46 -0500

    link to profile page

--- src/controllers/iframe.js
+++ src/controllers/iframe.js
@@ -2,12 +2,12 @@ import path from 'path';
 
 export function html(req, res, next) {
   let name = 'Anonymous';
+  let url = 'https://doenet.cloud/';
 
   if (req.jwt && req.jwt.user && !req.jwt.user.guest) {
     name = req.jwt.user.firstName;
+    url = `https://doenet.cloud/users/${req.jwt.user._id}`;
   }
-
-  const url = 'https://doenet.cloud/';
 
   res.set('Content-Type', 'text/html');
   res.send(`<html><head><script src="iframe.js"></script><base target="_parent"/></head><body style="overflow: hidden; margin: 0; font-family: sans-serif; height: 14pt; font-size: 12pt;"><a style="white-space: nowrap; width: 64pt; overflow: hidden; text-overflow: ellipsis; display: block;" href="${url}">${name}</a></body></html>`);
