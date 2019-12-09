ef1baf08992f4ce0d45b2892f30ad25165bd0638
Jim Fowler <fowler@math.osu.edu>
2019-12-08 21:08:42 -0500

    more error instrumentation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import app from './app';
 import mongoose from './config/mongoose';
 import client from './config/redis';
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Node server listening on port ${process.env.PORT}`);
+process.on('uncaughtException', (err, origin) => {
+  console.log(err);
+  console.log(err.stack);
+  process.exit(1);
 });
 
+process.on('unhandledRejection', function(err){
+  console.log(err.stack);
+  process.exit(1);
+});
+
+mongoose.connection.on('error', err => {
+  console.log(err);
+});
+
+mongoose.connection.on('connected', function () {
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`Node server listening on port ${process.env.PORT}`);
+  });
+});
